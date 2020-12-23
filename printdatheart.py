19030f2ec8feb2d999ec869a89da399eea679827
Dan Pickens <72703981+DanielPickens@users.noreply.github.com>
2020-12-23 15:44:16 -0700

    Create printdatheart.py

---
+++ printdatheart.py
@@ -0,0 +1,9 @@
+  def __init__(self,name, age):
+    self.name = name
+    self.Age = age
+
+  def myfunc(self): 
+    print("Here we go again *heart emoji " + self.name)
+
+p1 = Person("Dan", 30)
+p1.myfunc()
